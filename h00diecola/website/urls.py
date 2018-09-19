77454ca3fde36737aa855c3b2eb0c325fbfce5cc
h00die <michaeltcyr@gmail.com>
2018-09-18 21:29:30 -0400

    fix url for creating backups

--- h00diecola/website/urls.py
+++ h00diecola/website/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
   url(r'^login/$', views.login_view, name='login'),
   url(r'^logout/$', views.logout_view, name='logout'),
-  url(r'^create_backup/$', views.create_backup, name='create_backup'),
+  url(r'^create_backup$', views.create_backup, name='create_backup'),
   url(r'^queue_download/$', views.queue_download, name='queue_download'),
   url(r'^download/$', views.download, name='download'),
   url(r'^config/$', views.config_page, name='config_page'),
